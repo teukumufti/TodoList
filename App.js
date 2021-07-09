2327b4bfc87e991f5d32362fdc5560342561ee77
teukumufti <84464670+teukumufti@users.noreply.github.com>
2021-07-09 13:42:31 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import React, {useState} from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView, Platform } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+  Platform,
+} from 'react-native';
 import Task from './componen/Task';
 
 export default function App() {
@@ -8,59 +18,66 @@ export default function App() {
 
   const handleAddTask = () => {
     Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
 
-  const completeTask = (index) => {
+    if (task) {
+      setTaskItems([...taskItems, task]);
+      setTask(null);
+      alert(task);
+    } else {
+      alert('task is empty');
+    }
+  };
+
+  const completeTask = index => {
     let itemsCopy = [...taskItems];
     itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy)
-  }
+    setTaskItems(itemsCopy);
+  };
 
   return (
     <View style={styles.container}>
       {/* Added this scroll view to enable scrolling when list gets longer than the page */}
       <ScrollView
         contentContainerStyle={{
-          flexGrow: 1
+          flexGrow: 1,
         }}
-        keyboardShouldPersistTaps='handled'
-      >
-
-      {/* Today's Tasks */}
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
-        <View style={styles.items}>
-          {/* This is where the tasks will go! */}
-          {
-            taskItems.map((item, index) => {
+        keyboardShouldPersistTaps="handled">
+        {/* Today's Tasks */}
+        <View style={styles.tasksWrapper}>
+          <Text Text style={styles.sectionTitle}>
+            Today 's tasks
+          </Text>
+          <View style={styles.items}>
+            {/* This is where the tasks will go! */}
+            {taskItems.map((item, index) => {
               return (
-                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
-                  <Task text={item} /> 
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => completeTask(index)}>
+                  <Task text={item} />
                 </TouchableOpacity>
-              )
-            })
-          }
+              );
+            })}
+          </View>
         </View>
-      </View>
-        
       </ScrollView>
-
       {/* Write a task */}
       {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
-      <KeyboardAvoidingView 
-        behavior={Platform.ANDROID === "android" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+      <KeyboardAvoidingView
+        behavior={Platform.ANDROID === 'android' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task'}
+          value={task}
+          onChangeText={text => setTask(text)}
+        />
         <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
+            <Text style={styles.addText}> + </Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
-      
     </View>
   );
 }
@@ -68,7 +85,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: '#000000',
   },
   tasksWrapper: {
     paddingTop: 80,
@@ -76,7 +93,8 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    color: 'white',
   },
   items: {
     marginTop: 30,
@@ -87,7 +105,7 @@ const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   input: {
     paddingVertical: 15,
